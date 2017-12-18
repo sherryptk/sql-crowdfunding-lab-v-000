d30483e74723098ed150873dc9224efacb6d5472
Sherry El Garhy <sherryptk@gmail.com>
2017-12-18 23:21:05 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,11 @@ GROUP BY Projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+  "SELECT Users.name, Users.age, sum(Pledges.amount)
+  FROM Projects
+  INNER JOIN Pledges
+  ON Projects.id = Pledges.project_id
+  GROUP BY Users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
