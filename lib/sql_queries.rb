545b80a59b32c50f33c6ccad12ec42de84b7ad47
Sherry El Garhy <sherryptk@gmail.com>
2017-12-18 23:54:23 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,7 +43,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   FROM Projects
   INNER JOIN Pledges
   ON Projects.id = Pledges.user_id
-  WHERE Projects.category = "music";"
+  WHERE Projects.category = 'music' ; "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
