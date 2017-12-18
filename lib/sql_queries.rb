d660f476c905e2a3a22c4cf99eacf5705d91653d
Sherry El Garhy <sherryptk@gmail.com>
2017-12-18 23:17:25 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 FROM Projects
 INNER JOIN Pledges
 ON Projects.id = Pledges.project_id
-GROUP BY Projects.title HAVING sum(Pledges.amount);"
+ORDER BY Projects.title HAVING sum(Pledges.amount);"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
